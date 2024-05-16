6e1ca96789c09890aa3c9519aae8eaabbd641097
rachelsiw <rachelsiwinski@gmail.com>
2024-05-16 11:34:07 -0500

    Clear button and remove item

--- shopping-list-start/script.js
+++ shopping-list-start/script.js
@@ -1,6 +1,8 @@
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
+const clearBtn  = document.getElementById('clear');
+const itemFilter = document.getElementById('filter')
 
 
 function addItem (e) {
@@ -18,7 +20,10 @@ li.appendChild(document.createTextNode(newItem));
 
 const button = createButton('remove-item btn-link text-red');
 li.appendChild(button);
+
 itemList.appendChild(li);
+
+checkUI();
 itemInput.value = '';
 }
 
@@ -36,4 +41,35 @@ function createIcon (classes){
  return icon;
 }
 
+
+function removeItem(e){
+if (e.target.parentElement.classList.contains('remove-item')){
+  if(confirm('Are you sure?')){
+    e.target.parentElement.parentElement.remove();
+  }
+checkUI();
+}
+}
+
+function clearItems(e){
+while (itemList.firstChild){
+  itemList.removeChild(itemList.firstChild);
+}
+checkUI();
+}
+
+function checkUI(){
+  const items = itemList.querySelectorAll('li');
+if (items.length === 0){
+clearBtn.style.display = 'none';
+itemFilter.style.display = 'none';
+} else{
+  clearBtn.style.display = 'block';
+itemFilter.style.display = 'block';
+}
+}
+
 itemForm.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+clearBtn.addEventListener('click', clearItems);
+checkUI();
